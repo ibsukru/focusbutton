048279c9b78f77e5cebce291cac49dcb03b6b593
ibrahim velinov <ibsukru@gmail.com>
2024-11-30 03:31:58 +0100

    🚀

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,29 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
   headers: async () => {
     return [
       {
-        source: '/sw.js',
+        source: "/sw.js",
         headers: [
           {
-            key: 'Service-Worker-Allowed',
-            value: '/'
+            key: "Service-Worker-Allowed",
+            value: "/",
           },
           {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate'
-          }
-        ]
-      }
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
     ];
   },
   webpack: (config, { isServer }) => {
     // Add handling for web workers
     if (!isServer) {
-      config.output.globalObject = 'self';
+      config.output.globalObject = "self";
     }
     return config;
-  }
+  },
 };
 
 export default nextConfig;
