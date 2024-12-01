a40a68059eb76e1eb7b44ac588de528cba5aabeb
ibrahim velinov <ibsukru@gmail.com>
2024-12-01 02:04:00 +0100

    🚀

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -22,4 +22,28 @@ export default withPWA({
   // disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
+  buildExcludes: [
+    // Exclude Next.js specific files
+    /_next\/static\/.*/i,
+    /app-build-manifest.json/,
+    /build-manifest.json/,
+    // Add other files to exclude if needed
+  ],
+  // Only include specific files
+  include: ["/*", "/icons/*", "/timer-end.mp3"],
+  // Disable workbox logging in production
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/focusbutton\.com\/.*/i,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "focusbutton-cache",
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        },
+      },
+    },
+  ],
 })(nextConfig);
