d270061cd099d449dcded483fc2f63a08a8c88bb
ibrahim velinov <ibsukru@gmail.com>
2024-12-06 12:37:56 +0100

    🚀

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,40 +6,36 @@ import { getUserAgentInfo } from "../utils/userAgent";
 import styles from "./page.module.scss";
 
 export default async function Home() {
-  const isExtensionEnabled = false;
+  const isExtensionEnabled = true;
   const { isDesktop, isChrome, isFirefox } = await getUserAgentInfo();
 
   return (
     <div className={styles.page}>
-      <div className={styles.main}>
-        <FocusButton />
-        {(() => {
-          if (!isExtensionEnabled) return null;
-
-          if (!isDesktop) return null;
-
-          if (isChrome)
-            return (
-              <a
-                className={styles.webStore}
-                href="https://chrome.google.com/webstore/detail/focusbutton/njgjgjgjgjgjgjgjgjgj"
-              >
-                <FaChrome /> Download from Chrome Web Store
-              </a>
-            );
-
-          if (isFirefox)
-            return (
-              <a
-                className={styles.webStore}
-                href="https://addons.mozilla.org/en-US/firefox/addon/focusbutton/"
-              >
-                <FaFirefoxBrowser />
-                Download from Firefox Add-on Store
-              </a>
-            );
-        })()}
-      </div>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <FocusButton />
+            {isDesktop &&
+              !isExtensionEnabled &&
+              (isChrome ? (
+                <a
+                  className={styles.webStore}
+                  href="https://chromewebstore.google.com/detail/focusbutton/nkomoiomfaeodakglkihapminhpgnibl?authuser=0&hl=en&pli=1"
+                >
+                  <FaChrome /> Download from Chrome Web Store
+                </a>
+              ) : isFirefox ? (
+                <a
+                  className={styles.webStore}
+                  href="https://addons.mozilla.org/en-US/firefox/addon/focusbutton"
+                >
+                  <FaFirefoxBrowser />
+                  Download from Firefox Add-on Store
+                </a>
+              ) : null)}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
