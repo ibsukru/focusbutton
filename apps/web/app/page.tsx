f0effc35a894926fec3a027673ef452f93c8e9f5
ibrahim velinov <ibsukru@gmail.com>
2024-12-06 12:35:02 +0100

    Revert "🚀"
    This reverts commit 0ddb96735d1fb3792ad3c9f07cea98d64100a17b.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,8 +6,7 @@ import { getUserAgentInfo } from "../utils/userAgent";
 import styles from "./page.module.scss";
 
 export default async function Home() {
-  const isExtensionEnabled = true;
-
+  const isExtensionEnabled = false;
   const { isDesktop, isChrome, isFirefox } = await getUserAgentInfo();
 
   return (
@@ -23,7 +22,7 @@ export default async function Home() {
             return (
               <a
                 className={styles.webStore}
-                href="https://chromewebstore.google.com/detail/focusbutton/nkomoiomfaeodakglkihapminhpgnibl?authuser=0&hl=en&pli=1"
+                href="https://chrome.google.com/webstore/detail/focusbutton/njgjgjgjgjgjgjgjgjgj"
               >
                 <FaChrome /> Download from Chrome Web Store
               </a>
@@ -33,7 +32,7 @@ export default async function Home() {
             return (
               <a
                 className={styles.webStore}
-                href="https://addons.mozilla.org/en-US/firefox/addon/focusbutton"
+                href="https://addons.mozilla.org/en-US/firefox/addon/focusbutton/"
               >
                 <FaFirefoxBrowser />
                 Download from Firefox Add-on Store
