80d7b884bd48e99406d6877c29de04262b07e9c4
ibrahim velinov <ibsukru@gmail.com>
2025-01-05 11:29:10 +0100

    🚀

--- packages/ui/src/hooks/index.ts
+++ packages/ui/src/hooks/index.ts
@@ -1,35 +1,3 @@
-import { useState, useEffect } from "react";
+import useLocalStorage from "./useLocalStorage";
 
-export function useLocalStorage<T>(key: string, initialValue: T) {
-  const [storedValue, setStoredValue] = useState<T>(initialValue);
-
-  useEffect(() => {
-    try {
-      if (typeof window !== "undefined") {
-        const item = window.localStorage.getItem(key);
-        if (item) {
-          setStoredValue(JSON.parse(item));
-        }
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }, [key]);
-
-  const setValue = (value: T | ((val: T) => T)) => {
-    try {
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      setStoredValue(valueToStore);
-      if (typeof window !== "undefined") {
-        valueToStore !== null && valueToStore !== undefined
-          ? window.localStorage.setItem(key, JSON.stringify(valueToStore))
-          : window.localStorage.removeItem(key);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return [storedValue, setValue] as const;
-}
+export { useLocalStorage };
