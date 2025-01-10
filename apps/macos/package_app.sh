f3181df0327548216d9b0b7bdcea30097415b11a
ibrahim velinov <ibsukru@gmail.com>
2025-01-10 20:41:38 +0100

    🚀

--- apps/macos/package_app.sh
+++ apps/macos/package_app.sh
@@ -28,16 +28,16 @@ cp Sources/FocusButton/Resources/icon-128.png "$RESOURCES_DIR/"
 
 # Create icns from png
 mkdir -p icon.iconset
-sips -z 16 16 Sources/FocusButton/Resources/icon-128.png --out icon.iconset/icon_16x16.png
-sips -z 32 32 Sources/FocusButton/Resources/icon-128.png --out icon.iconset/icon_16x16@2x.png
-sips -z 32 32 Sources/FocusButton/Resources/icon-128.png --out icon.iconset/icon_32x32.png
-sips -z 64 64 Sources/FocusButton/Resources/icon-128.png --out icon.iconset/icon_32x32@2x.png
-sips -z 128 128 Sources/FocusButton/Resources/icon-128.png --out icon.iconset/icon_128x128.png
-sips -z 256 256 Sources/FocusButton/Resources/icon-128.png --out icon.iconset/icon_128x128@2x.png
-sips -z 256 256 Sources/FocusButton/Resources/icon-128.png --out icon.iconset/icon_256x256.png
-sips -z 512 512 Sources/FocusButton/Resources/icon-128.png --out icon.iconset/icon_256x256@2x.png
-sips -z 512 512 Sources/FocusButton/Resources/icon-128.png --out icon.iconset/icon_512x512.png
-sips -z 1024 1024 Sources/FocusButton/Resources/icon-128.png --out icon.iconset/icon_512x512@2x.png
+sips -z 16 16 Sources/FocusButton/Resources/icon-512.png --out icon.iconset/icon_16x16.png
+sips -z 32 32 Sources/FocusButton/Resources/icon-512.png --out icon.iconset/icon_16x16@2x.png
+sips -z 32 32 Sources/FocusButton/Resources/icon-512.png --out icon.iconset/icon_32x32.png
+sips -z 64 64 Sources/FocusButton/Resources/icon-512.png --out icon.iconset/icon_32x32@2x.png
+sips -z 128 128 Sources/FocusButton/Resources/icon-512.png --out icon.iconset/icon_128x128.png
+sips -z 256 256 Sources/FocusButton/Resources/icon-512.png --out icon.iconset/icon_128x128@2x.png
+sips -z 256 256 Sources/FocusButton/Resources/icon-512.png --out icon.iconset/icon_256x256.png
+sips -z 512 512 Sources/FocusButton/Resources/icon-512.png --out icon.iconset/icon_256x256@2x.png
+sips -z 512 512 Sources/FocusButton/Resources/icon-512.png --out icon.iconset/icon_512x512.png
+sips -z 1024 1024 Sources/FocusButton/Resources/icon-512.png --out icon.iconset/icon_512x512@2x.png
 iconutil -c icns icon.iconset -o "$RESOURCES_DIR/FocusButton.icns"
 rm -rf icon.iconset
 
